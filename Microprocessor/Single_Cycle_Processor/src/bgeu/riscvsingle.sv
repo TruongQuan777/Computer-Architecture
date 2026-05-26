aff5e98123be417ee662a33e63b24911f316ee53
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-05-26 15:51:36 +0800

    Update riscvsingle.sv

--- Microprocessor/Single_Cycle_Processor/src/bgeu/riscvsingle.sv
+++ Microprocessor/Single_Cycle_Processor/src/bgeu/riscvsingle.sv
@@ -5,17 +5,17 @@ module riscvsingle(input  logic        clk, reset,
                    output logic [31:0] ALUResult, WriteData,
                    input  logic [31:0] ReadData);
 
-  logic        ALUSrc, RegWrite, Jump, Zero;
+  logic        ALUSrc, RegWrite, Jump, Zero, CarryOut;
   logic [1:0] ResultSrc, ImmSrc;
   logic [2:0] ALUControl;
 
-  controller c(Instr[6:0], Instr[14:12], Instr[30], Zero,CarryOut
+  controller c(Instr[6:0], Instr[14:12], Instr[30], Zero,CarryOut,
                ResultSrc, MemWrite, PCSrc,
                ALUSrc, RegWrite, Jump,
                ImmSrc, ALUControl);
   datapath dp(clk, reset, ResultSrc, PCSrc,
               ALUSrc, RegWrite,
               ImmSrc, ALUControl,
-              Zero,Carryout PC, Instr,
+              Zero,CarryOut, PC, Instr,
               ALUResult, WriteData, ReadData);
 endmodule
