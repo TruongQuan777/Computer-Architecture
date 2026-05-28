17994281f4c86a4b2acf377737a79a63412dc45c
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-05-28 17:38:55 +0800

    Update slice.sv

--- Microprocessor/Single_Cycle_Processor/src/sb/slice.sv
+++ Microprocessor/Single_Cycle_Processor/src/sb/slice.sv
@@ -1,8 +1,8 @@
 module slice(input logic [1:0] MemSel, input logic [31:0] ReadData, input logic [31:0] WriteDataReg, output logic[31:0] WriteData);
   case (MemSel)
-    2'b00: WriteData=WriteDataReg;
+    2'b00: WriteData={ReadData[31:8],WriteDataReg[7:0]};
     2'b01: WriteData={ReadData[31:16],WriteDataReg[15:0]};
-    2'b10: WriteData={ReadData[31:8],WriteDataReg[7:0]};
+    2'b10: WriteData=WriteDataReg;
     default: WriteData=WriteDatareg;
   endcase
 endmodule
