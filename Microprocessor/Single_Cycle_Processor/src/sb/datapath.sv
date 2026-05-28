08a04e16600f5771f5459b5094c561a064f9520e
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-05-28 17:23:24 +0800

    Update datapath.sv

--- Microprocessor/Single_Cycle_Processor/src/sb/datapath.sv
+++ Microprocessor/Single_Cycle_Processor/src/sb/datapath.sv
@@ -3,6 +3,7 @@ module datapath(input  logic        clk, reset,
                 input  logic        PCSrc, ALUSrc,
                 input  logic        RegWrite,
                 input  logic [1:0]  ImmSrc,
+                input  logic [1:0]  MemSel, //Q
                 input  logic [2:0]  ALUControl,
                 output logic        Zero,
                 output logic [31:0] PC,
@@ -14,7 +15,7 @@ module datapath(input  logic        clk, reset,
   logic [31:0] ImmExt;
   logic [31:0] SrcA, SrcB;
   logic [31:0] Result;
-
+  logic [31:0] WriteDataReg; //Q
   // next PC logic
   flopr #(32) pcreg(clk, reset, PCNext, PC);
   adder       pcadd4(PC, 32'd4, PCPlus4);
@@ -23,12 +24,14 @@ module datapath(input  logic        clk, reset,
 
   // register file logic
   regfile     rf(clk, RegWrite, Instr[19:15], Instr[24:20],
-                 Instr[11:7], Result, SrcA, WriteData);
+                 Instr[11:7], Result, SrcA, WriteDataReg);//Q
   extend      ext(Instr[31:7], ImmSrc, ImmExt);
 
   // ALU logic
-  mux2 #(32)  srcbmux(WriteData, ImmExt, ALUSrc, SrcB);
+  mux2 #(32)  srcbmux(WriteDataReg, ImmExt, ALUSrc, SrcB);//Q
   alu         alu(SrcA, SrcB, ALUControl, ALUResult, Zero);
   mux3 #(32)  resultmux(ALUResult, ReadData, PCPlus4,
                         ResultSrc, Result);
+  //slice
+  slice sl(MemSel, ReadData, WriteDataReg, WriteData); //Q
 endmodule
