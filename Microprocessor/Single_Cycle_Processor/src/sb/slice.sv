6441c38fa79ef493e417c526ff4b87c97d41e731
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-05-29 00:16:56 +0800

    Update slice.sv

--- Microprocessor/Single_Cycle_Processor/src/sb/slice.sv
+++ Microprocessor/Single_Cycle_Processor/src/sb/slice.sv
@@ -4,14 +4,12 @@ module slice(
   input  logic [31:0] WriteDataReg, 
   output logic [31:0] WriteData
 );
-
-  // Wrap the case statement in an always_comb block
   always_comb begin
     case (MemSel)
       2'b00:   WriteData = {ReadData[31:8], WriteDataReg[7:0]};
       2'b01:   WriteData = {ReadData[31:16], WriteDataReg[15:0]};
       2'b10:   WriteData = WriteDataReg;
-      default: WriteData = WriteDataReg; // Fixed the capital 'R'
+      default: WriteData = WriteDataReg; 
     endcase
   end
 
