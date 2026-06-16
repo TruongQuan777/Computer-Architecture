c412a60913e38803919066b5a9d003d244c71c20
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-06-17 01:20:02 +0800

    Update riscvmulti.sv

--- Microprocessor/Multi_Cycle_Processor/src/riscvmulti.sv
+++ Microprocessor/Multi_Cycle_Processor/src/riscvmulti.sv
@@ -1,1 +1,14 @@
+ riscvmulti rvmulti(clk, reset, MemWrite, DataAdr, 
+                     WriteData, ReadData);
 
+logic PCWrite, AdrSrc,IRWrite;
+logic [1:0] ResultSrc;
+logic [2:0] ALUControl;
+logic [1:0] ALUSrcB,ALUSrcA,ImmSrc;
+logic RegWrite;
+logic [31:0] Instr;
+logic Zero;
+
+controller c(clk, PCwrite,AdrSrc, MemWrite, IRWrite, ResultSrc,ALUControl, ALUSrcB, ALUSrcA, ImmSrc, RegWrite, Instr[6:0],Instr[14:12],Instr[30],Zero);
+datapath dp(clk, reset, PCwrite,AdrSrc, IRWrite, ResultSrc,ALUControl, ALUSrcB, ALUSrcA, ImmSrc, RegWrite, Instr[6:0],Instr[14:12],Instr[30],Zero);
+endmodule
