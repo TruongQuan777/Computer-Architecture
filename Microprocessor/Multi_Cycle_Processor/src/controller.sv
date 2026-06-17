dec6a659823a2d308aa30be751240d377a49dbef
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-06-17 17:17:31 +0800

    Update controller.sv

--- Microprocessor/Multi_Cycle_Processor/src/controller.sv
+++ Microprocessor/Multi_Cycle_Processor/src/controller.sv
@@ -1,5 +1,12 @@
-module controller (input logic clk, output logic PCwrite, output logic AdrSrc, output logic MemWrite, output logic IRWrite, output logic [1:0] ResultSrc, output logic [2:0] ALUControl, 
-                   output logic [1:0] ALUSrcB, output logic [1:0] ALUSrcA, output logic [1:0] ImmSrc, output logic RegWrite, input logic [6:0] op, input logic [2:0] funct3, input logic funct7b5, input logic Zero);
+module controller (input logic clk, 
+                   output logic PCwrite, AdrSrc, MemWrite, IRWrite, 
+                   output logic [1:0] ResultSrc, 
+                   output logic [2:0] ALUControl, 
+                   output logic [1:0] ALUSrcB, ALUSrcA, ImmSrc, 
+                   output logic RegWrite, 
+                   input logic [6:0] op, 
+                   input logic [2:0] funct3, 
+                   input logic funct7b5, input logic Zero);
 
   logic [1:0] ALUOp;
   logic       Branch,PCUpdate;
