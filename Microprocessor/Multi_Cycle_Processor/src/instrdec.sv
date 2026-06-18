35079752c6cbfe9f26a824ce7bf5ab0353383493
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-06-18 15:20:06 +0800

    Update instrdec.sv

--- Microprocessor/Multi_Cycle_Processor/src/instrdec.sv
+++ Microprocessor/Multi_Cycle_Processor/src/instrdec.sv
@@ -5,13 +5,13 @@ module instrdec(
 
     always_comb begin
         case(op)
-            7'b0000011: immsrc = 2'b00; // lw
-            7'b0100011: immsrc = 2'b01; // sw
-            7'b0110011: immsrc = 2'bxx; // R-type (Don't care)
-            7'b1100011: immsrc = 2'b10; // beq
-            7'b0010011: immsrc = 2'b00; // I-type ALU
-            7'b1101111: immsrc = 2'b11; // jal
-            default:    immsrc = 2'bxx; // Default case for safety
+            7'b0000011: ImmSrc = 2'b00; // lw
+            7'b0100011: ImmSrc = 2'b01; // sw
+            7'b0110011: ImmSrc = 2'bxx; // R-type (Don't care)
+            7'b1100011: ImmSrc = 2'b10; // beq
+            7'b0010011: ImmSrc = 2'b00; // I-type ALU
+            7'b1101111: ImmSrc = 2'b11; // jal
+            default:    ImmSrc = 2'bxx; // Default case for safety
         endcase
     end
 
