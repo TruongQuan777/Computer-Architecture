b527372e5651b8251f9a2ea65259aa0c0410b8e7
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-06-18 11:58:40 +0800

    Update flopr32.sv

--- Microprocessor/Multi_Cycle_Processor/src/flopr32.sv
+++ Microprocessor/Multi_Cycle_Processor/src/flopr32.sv
@@ -1,8 +1,11 @@
-module flopr32(input  logic   clk, en, 
+module flopr32(input  logic   clk, reset, en, 
                  input  logic [31:0] d,
                  output logic [31:0] q);
 
   always_ff @(posedge clk)
-    if (en)   q <= d;
-    else  q <= q;
+    begin
+      if(reset) q<=0;
+      else q<=(en)?d:q;
+    end
+
 endmodule
