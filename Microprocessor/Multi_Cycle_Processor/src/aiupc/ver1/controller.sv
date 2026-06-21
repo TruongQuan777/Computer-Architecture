6f29e52a42bc3f670690de544d4866a52b2d24f5
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-06-21 17:18:37 +0800

    Create controller.sv

---
+++ Microprocessor/Multi_Cycle_Processor/src/aiupc/ver1/controller.sv
@@ -0,0 +1,18 @@
+module controller (input logic clk, 
+                   input logic reset,
+                   output logic PCWrite, AdrSrc, MemWrite, IRWrite, 
+                   output logic [1:0] ResultSrc, 
+                   output logic [2:0] ALUControl, 
+                   output logic [1:0] ALUSrcB, ALUSrcA, ImmSrc, 
+                   output logic RegWrite, 
+                   input logic [6:0] op, 
+                   input logic [2:0] funct3, 
+                   input logic funct7b5, input logic Zero);
+
+  logic [1:0] ALUOp;
+  logic       Branch,PCUpdate;
+  maindec md (clk, reset, op, ALUOp, Branch, PCUpdate, RegWrite, MemWrite, IRWrite, ResultSrc, ALUSrcB, ALUSrcA, AdrSrc);
+  aludec  ad(op[5], funct3, funct7b5, ALUOp, ALUControl);
+  instrdec id (op,ImmSrc);
+  assign PCWrite = Branch & Zero | PCUpdate;
+endmodule
